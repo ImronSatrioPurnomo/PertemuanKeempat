36c9ab7a1e385bad6d6808ff2a4b9ce763d57ecd
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-23 15:36:43 +0700

    memanggil Activitas pertama di main activity

--- app/src/main/java/com/example/pertemuankeempat/MainActivity.kt
+++ app/src/main/java/com/example/pertemuankeempat/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PertemuanKeempatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
